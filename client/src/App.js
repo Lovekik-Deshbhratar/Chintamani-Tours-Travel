1c9b86fc6d542b836d8ece3a7b81ac95f5c6aea5
Lovekik <lovekikdeshbhratar@gmail.com>
2023-07-22 15:37:18 +0530

    Navbar Done

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Landing from "./Pages/Landing";
 
 function App() {
   return (
-    <div>
-      {" "}
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="w-full">
+      <Routes>
+        <Route exact path="/" element={<Landing />} />
+        {/* <Route path="/about" element={} />
+        <Route path="/services" element={} />
+        <Route path="/contact" element={} />
+        <Route path="*" element={<Home />} /> */}
+      </Routes>
     </div>
   );
 }
---
+++ client/src/Component/Navbar.jsx
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { Menu, X } from "lucide-react";
+
+const NavLinks = () => {
+  return (
+    <>
+      <NavLink
+        className={({ isActive }) =>
+          isActive
+            ? "text-primary text-xl hover:text-primary"
+            : "text-gray-500 text-lg hover:text-primary"
+        }
+        to={"/"}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        className={({ isActive }) =>
+          isActive
+            ? "text-primary text-xl hover:text-primary"
+            : "text-gray-500 text-lg hover:text-primary"
+        }
+        to={"/services"}
+      >
+        Services
+      </NavLink>
+      <NavLink
+        className={({ isActive }) =>
+          isActive
+            ? "text-primary text-xl hover:text-primary"
+            : "text-gray-500 text-lg hover:text-primary"
+        }
+        to={"/about"}
+      >
+        About
+      </NavLink>
+      <NavLink
+        className={({ isActive }) =>
+          isActive
+            ? "text-primary text-xl hover:text-primary"
+            : "text-gray-500 text-lg hover:text-primary"
+        }
+        to={"/contact"}
+      >
+        Contact
+      </NavLink>
+    </>
+  );
+};
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <div className="pt-4 pb-4">
+      <div className="md:flex md:justify-between md:mx-[15rem] space-y-5">
+        <div className="flex justify-between px-4 md:p-0">
+          <button>
+            <img
+              className="w-[9rem] md:w-24 "
+              src="https://www.chintamanitours.com/static/img/logo/chintamani.png"
+              alt=""
+            />
+          </button>
+          <div className="flex items-center">
+            {isOpen ? (
+              <X onClick={handleToggle} className="md:hidden" size={35} />
+            ) : (
+              <Menu onClick={handleToggle} className="md:hidden" size={35} />
+            )}
+          </div>
+        </div>
+        <div className="hidden md:flex gap-6 md:flex-row flex-col items-center">
+          <NavLinks />
+        </div>
+        {isOpen && (
+          <div className="flex gap-5 md:flex-row flex-col md:hidden  items-center">
+            <NavLinks />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
