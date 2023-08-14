d8e8546f4f19dcfe987d802b87f7de9cbc998b7a
Lovekik <lovekikdeshbhratar@gmail.com>
2023-08-14 19:59:54 +0530

    about and contact page done

--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -1,12 +1,34 @@
 import React from "react";
 import Navbar from "../Component/Navbar";
 import Footer from "../Component/Footer";
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
     <>
       <Navbar />
-      <div>About</div>
+      <div className="xl:px-44 lg:px-20 lg:h-[80vh] flex flex-col lg:flex-row md:items-center md:justify-between px-4 gap-10 lg:gap-0 mt-6 lg:mt-0">
+        <div className="flex flex-col lg:w-fit gap-7">
+          <h1 className="text-3xl lg:text-4xl text-black tracking-wide font-[600] lg:w-[35rem] leading-tight">
+            Welcome to About page of{" "}
+            <span className="text-primary capitalize">
+              Chintamani tours & travel
+            </span>
+          </h1>
+          <h1 className="text-gray-700 text-xl">
+            We are team of talanted developer making websites
+          </h1>
+          <Link
+            to={"/contact"}
+            className="border-2 border-secondary text-primary rounded-3xl px-8 py-2.5 w-fit hover:text-white hover:bg-secondary transition ease-in-out duration-500 tracking-wider text-sm"
+          >
+            Contact Here
+          </Link>
+        </div>
+        <div>
+          <img src="/Asset/aboutImage.svg" className="animate-up-down" />
+        </div>
+      </div>
       <Footer />
     </>
   );
--- client/src/Pages/Contact.jsx
+++ client/src/Pages/Contact.jsx
@@ -6,7 +6,52 @@ const Contact = () => {
   return (
     <>
       <Navbar />
-      <div>Contact</div>
+      <div className="flex flex-col items-center my-10">
+        <h1 className="text-4xl text-gray-800">Contact Us</h1>
+        <span className="capitalize text-gray-700">
+          Contact us if u have any query
+        </span>
+        <div className="lg:w-[40rem] flex flex-col gap-5 mt-14 w-full px-6 md:px-36 lg:px-0">
+          <div>
+            <h1>Full Name</h1>
+            <input
+              type="text"
+              required
+              className="border mt-2 outline-none rounded-[0.19rem] w-full px-3 py-1.5 text-gray-800 placeholder:text-gray-400 placeholder:text-base placeholder:font-thin focus:border-4 focus:border-secondary/40 focus:border-opacity-50 transition-all duration-75"
+              placeholder="name"
+            />
+          </div>
+          <div>
+            <h1>Phone</h1>
+            <input
+              type="text"
+              required
+              className="border mt-2 outline-none rounded-[0.19rem] w-full px-3 py-1.5 text-gray-800 placeholder:text-gray-400 placeholder:text-base placeholder:font-thin focus:border-4 focus:border-secondary/40 focus:border-opacity-50 transition-all duration-75 "
+              placeholder="mobile number"
+            />
+          </div>
+          <div>
+            <h1>Email address</h1>
+            <input
+              type="text"
+              required
+              className="border mt-2 outline-none rounded-[0.19rem] w-full px-3 py-1.5 text-gray-800 placeholder:text-gray-400 placeholder:text-base placeholder:font-thin focus:border-4 focus:border-secondary/40 focus:border-opacity-50 transition-all duration-75"
+              placeholder="name@example.com"
+            />
+          </div>{" "}
+          <div>
+            <h1>Message</h1>
+            <textarea
+              placeholder="Optional"
+              className="border mt-2 outline-none rounded-[0.19rem] w-full px-3 py-1.5 text-gray-800 resize-none focus:border-4 focus:border-secondary/40 focus:border-opacity-50 transition-all duration-75"
+              rows={4}
+            ></textarea>
+          </div>
+          <button className="mt-7 h-fit w-fit border border-secondary text-sm px-3 py-1.5 text-primary rounded-[0.2rem] hover:bg-secondary hover:text-white transition ease-in-out duration-500 ">
+            Submit Form
+          </button>
+        </div>
+      </div>
       <Footer />
     </>
   );
