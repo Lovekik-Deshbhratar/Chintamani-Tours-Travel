9214d255c43ce6084e622823d9ee2ce1b311eb3b
Lovekik <lovekikdeshbhratar@gmail.com>
2023-08-21 18:12:17 +0530

    added html text in booking email

--- server/routes/BookingRoute.js
+++ server/routes/BookingRoute.js
@@ -33,22 +33,64 @@ BookingRoute.post("/", verifyUser, async (req, res) => {
       from: process.env.EMAIL_USER,
       to: userEmail,
       subject: `Tour Booking Confirmation`,
-      text: `Dear ${userEmail},
-
-      We are excited to confirm your booking for the ${tourName} on ${bookAt}. Your booking details are as follows:
-      
-      Tour Name: ${tourName}
-      Booking Date: ${bookAt}
-      Number of People: ${peopleSize}
-      Tour Price X Number of people: ${amountXprice}
-      Service Charges: ${serviceCharges}
-      Grand Total: ${total}
-      
-      Your booking is now confirmed, and we look forward to having you on the tour. If you have any questions or need further assistance, please feel free to contact our support team.
-      
-      Thank you for choosing us for your travel experience!
-      
-      Best regards`,
+      html: `<!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Tour Booking Confirmation</title>
+          <style>
+              body {
+                  font-family: Arial, sans-serif;
+                  margin: 0;
+                  padding: 0;
+              }
+              .container {
+                  max-width: 600px;
+                  margin: 0 auto;
+                  padding: 20px;
+                  border: 1px solid #ddd;
+                  border-radius: 10px;
+                  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+              }
+              h1 {
+                  font-size: 24px;
+                  margin-bottom: 20px;
+              }
+              p {
+                  font-size: 16px;
+                  line-height: 1.5;
+              }
+              .highlight {
+                  font-weight: bold;
+              }
+          </style>
+      </head>
+      <body>
+          <div class="container">
+              <h1>Tour Booking Confirmation</h1>
+              <p>Dear <span class="highlight">${userEmail}</span>,</p>
+              
+              <p>We are excited to confirm your booking for the <span class="highlight">${tourName}</span> on <span class="highlight">${bookAt}</span>. Your booking details are as follows:</p>
+              
+              <p>
+                  <strong>Tour Name:</strong> ${tourName}<br>
+                  <strong>Booking Date:</strong> ${bookAt}<br>
+                  <strong>Number of People:</strong> ${peopleSize}<br>
+                  <strong>Tour Price X Number of people:</strong> ${amountXprice}<br>
+                  <strong>Service Charges:</strong> ${serviceCharges}<br>
+                  <strong>Grand Total:</strong> ${total}
+              </p>
+              
+              <p>Your booking is now confirmed, and we look forward to having you on the tour. If you have any questions or need further assistance, please feel free to contact our support team.</p>
+              
+              <p>Thank you for choosing us for your travel experience!</p>
+              
+              <p>Best regards,</p>
+          </div>
+      </body>
+      </html>
+      `,
     };
 
     transporter.sendMail(mailOptions, (error, info) => {
