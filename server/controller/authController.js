a874100ba7d461386104c0ad1f59ba1f6dfd9194
Lovekik <lovekikdeshbhratar@gmail.com>
2023-08-23 16:50:47 +0530

    testing 4

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -100,20 +100,17 @@ export const login = async (req, res) => {
         { expiresIn: "15d" }
       );
 
-      const cookieOptions = {
-        expires: new Date(Date.now() + 15 * 24 * 60 * 60 * 1000),
-        httpOnly: true,
-        secure: true,
-        sameSite: "strict",
-      };
-
       // Set token in the browser cookies and send responce to the client
       res
-        .cookie("accessToken", token, cookieOptions)
+        .cookie("accessToken", token, {
+          httpOnly: true,
+          expires: token.expiresIn,
+          secure: true,
+        })
         .status(200)
         .json({
           token,
-          message: "Login successful",
+          message: "Login successfull",
           data: { ...rest },
           role,
         });
